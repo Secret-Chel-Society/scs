4440302ed1ffff4f3c1366c75976063cc2ff8a5f
DarkWolf <163810802+ZachariaJ24@users.noreply.github.com>
2025-08-22 11:47:12 -0400

    Update mghl-logo.tsx

--- components/layout/mghl-logo.tsx
+++ components/layout/mghl-logo.tsx
@@ -13,7 +13,7 @@ export function MGHLLogo({ onClick }: MGHLLogoProps) {
     <Link href="/" onClick={onClick}>
       <div className="flex items-center">
         <Image
-          src="https://kudmtqjzuxakngbrqxzp.supabase.co/storage/v1/object/public/logoheader/scslogo.png"
+          src="https://kudmtqjzuxakngbrqxzp.supabase.co/storage/v1/object/public/media/scslogo.png"
           alt="SCS Logo"
           width={120}
           height={40}
