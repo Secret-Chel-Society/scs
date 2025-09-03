604231567724cb7d855de61d6a1a38c001da8526
DarkWolf <163810802+ZachariaJ24@users.noreply.github.com>
2025-09-03 16:29:13 -0400

    bidding recap revamp

--- app/free-agency/bidding-recap/page.tsx
+++ app/free-agency/bidding-recap/page.tsx
@@ -2,8 +2,10 @@ import { PublicBiddingRecap } from "@/components/free-agency/public-bidding-reca
 
 export default function BiddingRecapPage() {
   return (
-    <div className="container mx-auto py-8">
-      <PublicBiddingRecap />
+    <div className="min-h-screen bg-gradient-to-br from-ice-blue-50 via-white to-rink-blue-50 dark:from-hockey-silver-900 dark:via-hockey-silver-800 dark:to-rink-blue-900/30">
+      <div className="container mx-auto py-8">
+        <PublicBiddingRecap />
+      </div>
     </div>
   )
 }
