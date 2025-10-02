3e17d8d6def093d377bf4d3cf6f5d9b17b438b48
DarkWolf <163810802+ZachariaJ24@users.noreply.github.com>
2025-10-02 16:48:31 -0400

    updateupdate

--- components/providers/mantine-provider.tsx
+++ components/providers/mantine-provider.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from 'react'
 import { MantineProvider } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import { ModalsProvider } from '@mantine/modals'
