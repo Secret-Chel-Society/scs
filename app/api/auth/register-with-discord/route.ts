647c86e48f94cbb4008a778b3af39d1b16293cc2
LispDoge <88755957+LispDoge@users.noreply.github.com>
2025-12-09 10:06:19 -0500

    Update route.ts

--- app/api/auth/register-with-discord/route.ts
+++ app/api/auth/register-with-discord/route.ts
@@ -1,9 +1,9 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { createRouteHandlerClient } from "@/lib/supabase/server"
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
 import type { NextRequest } from "next/server"
-import type { Database } from "@/lib/database.types"
+import type { Database } from "@/lib/types/database"
 
 export const dynamic = "force-dynamic"
 
